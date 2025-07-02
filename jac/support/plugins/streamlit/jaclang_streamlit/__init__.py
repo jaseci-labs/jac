025abd445df9af38f72ea12390200728e5970cd7
udithishanka <udithishanka.s@gmail.com>
2025-07-02 08:39:02 +0530

    Refactor run_streamlit function to improve plugin registration and import handling

--- jac/support/plugins/streamlit/jaclang_streamlit/__init__.py
+++ jac/support/plugins/streamlit/jaclang_streamlit/__init__.py
@@ -1,15 +1,21 @@
 """Streamlit of Jac."""
 
 from jaclang_streamlit.app_test import JacAppTest as AppTest
+from jaclang.runtimelib.machine import plugin_manager
 
 
 def run_streamlit(basename: str, dirname: str) -> None:
     """Run the Streamlit application."""
-    from jaclang.runtimelib.machine import JacMachineInterface
+    import jaclang.runtimelib.machine as machine
 
-    JacMachineInterface.jac_import(
-        basename, base_path=dirname, reload_module=True
-    )  # TODO: need flag to force reload here
+    if "JacMachineInterfaceImpl" not in plugin_manager._name2plugin:
+        plugin_manager.register(machine.JacMachineInterfaceImpl)
+
+    machine.JacMachineInterface.jac_import(
+        basename,
+        base_path=dirname,
+        reload_module=True
+    )
 
 
 __all__ = ["AppTest", "run_streamlit"]
