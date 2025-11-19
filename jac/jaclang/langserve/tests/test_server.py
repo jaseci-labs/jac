018e897edddc707897db437c49536787d0cb402b
kugesan1105 <kugesan.sivasothynathan@jaseci.org>
2025-11-20 00:47:34 +0530

    Remove unnecessary blank line in test_server.py

--- jac/jaclang/langserve/tests/test_server.py
+++ jac/jaclang/langserve/tests/test_server.py
@@ -13,7 +13,6 @@ from jaclang.langserve.engine import JacLangServer
 # Enable tracemalloc to track resource allocations
 tracemalloc.start()
 
-
 class TestJacLangServer:
     """Test suite for JacLangServer functionality."""
 
