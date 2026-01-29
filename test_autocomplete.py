a28ac37e824df622087400f74bb0bf33086d2e9b
SandeepaHWP <sandeepahwp.22@uom.lk>
2026-01-29 16:52:15 +0530

    chore: remove obsolete autocomplete and import test scripts

--- test_autocomplete.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-from jaclang.vendor import jaccomplete
-
-p = argparse.ArgumentParser()
-sub = p.add_subparsers()
-sub.add_parser("foo")
-jaccomplete.autocomplete(p)
-print("Autocomplete returned")
